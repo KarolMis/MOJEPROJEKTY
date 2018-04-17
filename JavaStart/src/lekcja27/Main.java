54bd30e9d5c37f048ceebfe4ff03744bd5d7beaf
KarolMis <sunziboks@gmail.com>
2018-04-17 14:15:25 +0200

    new lessons

---
+++ JavaStart/src/lekcja27/Main.java
@@ -0,0 +1,11 @@
+package lekcja27;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(factorial(10));
+    }
+
+    private static int factorial(int n){
+        return n>1? n * factorial(n-1) : n;
+    }
+}
