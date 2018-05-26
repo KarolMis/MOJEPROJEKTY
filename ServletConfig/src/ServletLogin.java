eb99835eb493d997e015fb26b5e90acd62eb1e7e
KarolMis <sunziboks@gmail.com>
2018-05-26 10:10:30 +0200

    new lessons

--- ServletConfig/src/ServletLogin.java
+++ ServletConfig/src/ServletLogin.java
@@ -1,17 +1,25 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "ServletLogin")
+@WebServlet(urlPatterns = "/ServletLogin", initParams = {@WebInitParam(name = "defaultUsername", value = "Nieznajomy")})
 public class ServletLogin extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
 
+        String username = request.getParameter("username");
+        if(username == null || "".equals(username)) {
+            username = getServletConfig().getInitParameter("defaultUsername");
+        } else {
+            request.getSession().setAttribute("username", username);
+        }
+
+        writer.println("Witaj " + username);
     }
 }
---
+++ ServletConfig/web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: karol
+  Date: 23.05.2018
+  Time: 09:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+  <body>
+  <form method="get" action="ServletLogin">
+    Nazwa użytkownika: <br>
+    <input type="text" name="username"><br>
+    <input type="submit" value="Wyślij">
+  </form>
+  </body>
+</html>
